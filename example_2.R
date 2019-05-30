887ca81e56074f6982d27ec55a3810afd2b647cb
richelbilderbeek <richel@richelbilderbeek.nl>
2019-05-30 09:21:29 +0200

    Run script

--- example_2.R
+++ example_2.R
@@ -1,103 +1,103 @@
-  # Code of example 2
-  #
-  # Works under Linux and MacOS only
-  suppressMessages(library(ggtree))
-  suppressMessages(library(ggplot2))
-  library(pirouette)
-  library(babette)
-  library(beautier)
+# Code of example 2
+#
+# Works under Linux and MacOS only
+suppressMessages(library(ggtree))
+suppressMessages(library(ggplot2))
+library(pirouette)
+library(babette)
+library(beautier)
 
 
-  root_folder <- path.expand("~/GitHubs/pirouette_article")
-  example_no <- 2
-  example_folder <- file.path(root_folder, paste0("example_", example_no))
-  dir.create(example_folder, showWarnings = FALSE)
-  setwd(example_folder)
-  set.seed(314)
-  testit::assert(is_beast2_installed())
-  phylogeny <- create_yule_tree(n_taxa = 6, crown_age = 10)
+root_folder <- path.expand("~/GitHubs/pirouette_article")
+example_no <- 2
+example_folder <- file.path(root_folder, paste0("example_", example_no))
+dir.create(example_folder, showWarnings = FALSE, recursive = TRUE)
+setwd(example_folder)
+set.seed(314)
+testit::assert(is_beast2_installed())
+phylogeny <- create_yule_tree(n_taxa = 6, crown_age = 10)
 
-  alignment_params <- create_alignment_params(
-    root_sequence = create_blocked_dna(length = 1000),
-    mutation_rate = 0.1
+alignment_params <- create_alignment_params(
+  root_sequence = create_blocked_dna(length = 1000),
+  mutation_rate = 0.1
+)
+
+# JC69, strict, Yule
+generative_experiment <- create_experiment(
+  inference_conditions = create_inference_conditions(
+    model_type = "generative",
+    run_if = "always",
+    do_measure_evidence = TRUE
+  ),
+  inference_model = create_inference_model(
+    site_model = create_jc69_site_model(),
+    clock_model = create_strict_clock_model(),
+    tree_prior = create_yule_tree_prior(),
+    mcmc = create_mcmc(chain_length = 1e+07, store_every = 1000)
   )
+)
+generative_experiment <- create_gen_experiment()
+check_experiment(generative_experiment)
 
-  # JC69, strict, Yule
-  generative_experiment <- create_experiment(
-    inference_conditions = create_inference_conditions(
-      model_type = "generative",
-      run_if = "always",
-      do_measure_evidence = TRUE
-    ),
-    inference_model = create_inference_model(
-      site_model = create_jc69_site_model(),
-      clock_model = create_strict_clock_model(),
-      tree_prior = create_yule_tree_prior(),
-      mcmc = create_mcmc(chain_length = 1e+07, store_every = 1000)
+# All non-Yule tree priors
+candidate_experiments <- create_all_experiments(
+  exclude_model = generative_experiment$inference_model
+)
+check_experiments(candidate_experiments)
+
+experiments <- c(list(generative_experiment), candidate_experiments)
+check_experiments(experiments)
+
+# Testing
+if (1 == 2) {
+  experiments <- experiments[1:2]
+  for (i in seq_along(experiments)) {
+    experiments[[i]]$inference_model$mcmc <- create_mcmc(chain_length = 10000, store_every = 1000)
+    experiments[[i]]$est_evidence_mcmc <- create_mcmc_nested_sampling(
+      chain_length = 10000,
+      store_every = 1000,
+      epsilon = 100.0
     )
-  )
-  generative_experiment <- create_gen_experiment()
-  check_experiment(generative_experiment)
-
-  # All non-Yule tree priors
-  candidate_experiments <- create_all_experiments(
-    exclude_model = generative_experiment$inference_model
-  )
-  check_experiments(candidate_experiments)
-
-  experiments <- c(list(generative_experiment), candidate_experiments)
-  check_experiments(experiments)
-
-  # Testing
-  if (1 == 2) {
-    experiments <- experiments[1:2]
-    for (i in seq_along(experiments)) {
-      experiments[[i]]$inference_model$mcmc <- create_mcmc(chain_length = 10000, store_every = 1000)
-      experiments[[i]]$est_evidence_mcmc <- create_mcmc_nested_sampling(
-        chain_length = 10000,
-        store_every = 1000,
-        epsilon = 100.0
-      )
-    }
   }
+}
 
-  pir_params <- create_pir_params(
-    alignment_params = alignment_params,
-    experiments = experiments
-  )
+pir_params <- create_pir_params(
+  alignment_params = alignment_params,
+  experiments = experiments
+)
 
-  print("#######################################################################")
-  print("Settings to run on Peregrine cluster")
-  print("#######################################################################")
-  pir_params$alignment_params$fasta_filename <- file.path(example_folder, "true.fasta")
-  for (i in seq_along(pir_params$experiments)) {
-    pir_params$experiments[[i]]$beast2_options$input_filename <- file.path(example_folder, "beast2_input_best.xml")
-    pir_params$experiments[[i]]$beast2_options$output_log_filename <- file.path(example_folder, "beast2_output_best.log")
-    pir_params$experiments[[i]]$beast2_options$output_trees_filenames <- file.path(example_folder, "beast2_output_best.trees")
-    pir_params$experiments[[i]]$beast2_options$output_state_filename <- file.path(example_folder, "beast2_output_best.xml.state")
-    pir_params$experiments[[i]]$beast2_options$beast2_working_dir <- example_folder
-    pir_params$experiments[[i]]$errors_filename <- file.path(example_folder, "error_best.csv")
-  }
-  pir_params$experiments[[1]]$beast2_options$input_filename <- file.path(example_folder, "beast2_input_gen.xml")
-  pir_params$experiments[[1]]$beast2_options$output_log_filename <- file.path(example_folder, "beast2_output_gen.log")
-  pir_params$experiments[[1]]$beast2_options$output_trees_filenames <- file.path(example_folder, "beast2_output_gen.trees")
-  pir_params$experiments[[1]]$beast2_options$output_state_filename <- file.path(example_folder, "beast2_output_gen.xml.state")
-  pir_params$experiments[[1]]$errors_filename <- file.path(example_folder, "error_gen.csv")
-  pir_params$evidence_filename <- file.path(example_folder, "evidence_true.csv")
-  if (!is_one_na(pir_params$twinning_params)) {
-    pir_params$twinning_params$twin_tree_filename <- file.path(example_folder, "twin.tree")
-    pir_params$twinning_params$twin_alignment_filename <- file.path(example_folder, "twin.fasta")
-    pir_params$twinning_params$twin_evidence_filename <- file.path(example_folder, "evidence_twin.csv")
-  }
-  rm_pir_param_files(pir_params)
-  print("#######################################################################")
+print("#######################################################################")
+print("Settings to run on Peregrine cluster")
+print("#######################################################################")
+pir_params$alignment_params$fasta_filename <- file.path(example_folder, "true.fasta")
+for (i in seq_along(pir_params$experiments)) {
+  pir_params$experiments[[i]]$beast2_options$input_filename <- file.path(example_folder, "beast2_input_best.xml")
+  pir_params$experiments[[i]]$beast2_options$output_log_filename <- file.path(example_folder, "beast2_output_best.log")
+  pir_params$experiments[[i]]$beast2_options$output_trees_filenames <- file.path(example_folder, "beast2_output_best.trees")
+  pir_params$experiments[[i]]$beast2_options$output_state_filename <- file.path(example_folder, "beast2_output_best.xml.state")
+  pir_params$experiments[[i]]$beast2_options$beast2_working_dir <- example_folder
+  pir_params$experiments[[i]]$errors_filename <- file.path(example_folder, "error_best.csv")
+}
+pir_params$experiments[[1]]$beast2_options$input_filename <- file.path(example_folder, "beast2_input_gen.xml")
+pir_params$experiments[[1]]$beast2_options$output_log_filename <- file.path(example_folder, "beast2_output_gen.log")
+pir_params$experiments[[1]]$beast2_options$output_trees_filenames <- file.path(example_folder, "beast2_output_gen.trees")
+pir_params$experiments[[1]]$beast2_options$output_state_filename <- file.path(example_folder, "beast2_output_gen.xml.state")
+pir_params$experiments[[1]]$errors_filename <- file.path(example_folder, "error_gen.csv")
+pir_params$evidence_filename <- file.path(example_folder, "evidence_true.csv")
+if (!is_one_na(pir_params$twinning_params)) {
+  pir_params$twinning_params$twin_tree_filename <- file.path(example_folder, "twin.tree")
+  pir_params$twinning_params$twin_alignment_filename <- file.path(example_folder, "twin.fasta")
+  pir_params$twinning_params$twin_evidence_filename <- file.path(example_folder, "evidence_twin.csv")
+}
+rm_pir_param_files(pir_params)
+print("#######################################################################")
 
-  Sys.time()
-  errors <- pir_run(
-    phylogeny,
-    pir_params = pir_params
-  )
-  Sys.time()
+Sys.time()
+errors <- pir_run(
+  phylogeny,
+  pir_params = pir_params
+)
+Sys.time()
 
 if (1 == 2) {
   errors <- utils::read.csv(
